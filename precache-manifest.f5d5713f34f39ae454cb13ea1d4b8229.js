7b865104bb72a73b5f5365497042010f05d95be7
Sergey Bovyrin <sbovyrin@gmail.com>
2020-11-17 22:31:18 +0300

    demo

---
+++ precache-manifest.f5d5713f34f39ae454cb13ea1d4b8229.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53dfd1df5a53b08e17a49cfa9bd5fb3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bc4ffce66c0cfba4132",
+    "url": "/static/css/main.83965a0f.chunk.css"
+  },
+  {
+    "revision": "682691b82120194b7a8b",
+    "url": "/static/js/2.d3155f85.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.d3155f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc4ffce66c0cfba4132",
+    "url": "/static/js/main.1debd92f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "e6ac8f893b4628533167a3e5d7723e80",
+    "url": "/static/media/111.e6ac8f89.jpg"
+  },
+  {
+    "revision": "60ea48006f981b89e45f47a54de627f3",
+    "url": "/static/media/catering.60ea4800.jpg"
+  },
+  {
+    "revision": "cec42d36ac2f09e02d314941c5b16f08",
+    "url": "/static/media/full_logo.cec42d36.svg"
+  },
+  {
+    "revision": "1d8ea208ed0a76df49dc52419a43a04b",
+    "url": "/static/media/pen.1d8ea208.svg"
+  },
+  {
+    "revision": "e5173e27010e629acba81616493d08a4",
+    "url": "/static/media/product.e5173e27.jpg"
+  },
+  {
+    "revision": "e0c909f403ce8e8fcb4aaab1970c047e",
+    "url": "/static/media/red_caviar_sample.e0c909f4.png"
+  },
+  {
+    "revision": "95a476d56750f114a3ac4fcd4b0f0874",
+    "url": "/static/media/shopping-bag.95a476d5.svg"
+  },
+  {
+    "revision": "c3335cd795618f69197630024ba30611",
+    "url": "/static/media/short_logo.c3335cd7.svg"
+  },
+  {
+    "revision": "4bd840ace55dbeaeb0905b2131102c36",
+    "url": "/static/media/truffles_background.4bd840ac.jpg"
+  },
+  {
+    "revision": "aca121214883d8f189db1d2fdad1d083",
+    "url": "/static/media/user.aca12121.svg"
+  }
+]);
